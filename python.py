fa70c904071182795459db98dbda1095a497aec5
deviikkaa <devikasunil925@gmail.com>
2025-01-17 12:25:20 +0530

    First Commit

--- python.py
+++ python.py
@@ -1,9 +1,9 @@
 # Store input numbers
 num1 = input('Enter first number: ')
-num2 = input 'Enter second number: ')
-
+num2 = input ('Enter second number: ')
+num3 = input ('Enter third number:')
 # Add two numbers
-sum = float(num1 + float 
+sum = float(num1 + float) 
 
 # Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum of {0} and {1} and {2} is {3}'.format(num1, num2, sum))
