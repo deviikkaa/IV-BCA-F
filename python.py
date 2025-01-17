b418b07d67338f7bd8de1062c078d054acec62f9
deviikkaa <devikasunil925@gmail.com>
2025-01-17 12:30:26 +0530

    Update python.py

--- python.py
+++ python.py
@@ -3,7 +3,7 @@ num1 = input('Enter first number: ')
 num2 = input ('Enter second number: ')
 num3 = input ('Enter third number:')
 # Add two numbers
-sum = float(num1 + num2) 
+sum = float(num1 + num2 + num3) 
 
 # Display the sum
 print('The sum of {0} and {1} and {2} is {3}'.format(num1, num2, sum))
